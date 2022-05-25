00f3c4d62ac9b9533e2af59479fa6fbfa5956b79
Fogel Egor <fogel_92@bk.ru>
2022-05-25 23:35:55 +0700

    feat: make a JS aim training game

---
+++ app.js
@@ -0,0 +1,87 @@
+const startBtn = document.querySelector("#start")
+const screens = document.querySelectorAll(".screen")
+const timeList = document.querySelector("#time-list")
+const timeEl = document.querySelector("#time")
+const board = document.querySelector("#board")
+const colors = [
+  "linear-gradient(to right, #43e97b 0%, #38f9d7 100%)",
+  "linear-gradient(to right, #4facfe 0%, #00f2fe 100%)",
+  "linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%)",
+]
+let time = 0
+let score = 0
+
+startBtn.addEventListener("click", (event) => {
+  event.preventDefault()
+  screens[0].classList.add("up")
+})
+
+timeList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("time-btn")) {
+    time = parseInt(event.target.getAttribute("data-time"))
+    screens[1].classList.add("up")
+    startGame()
+  }
+})
+
+board.addEventListener("click", (event) => {
+  if (event.target.classList.contains("circle")) {
+    score++
+    event.target.remove()
+    createRandomCircle()
+  }
+})
+
+function startGame() {
+  setInterval(decreaseTime, 1000)
+  createRandomCircle()
+  setTime(time)
+}
+
+function decreaseTime() {
+  if (time === 0) {
+    finishGame()
+  } else {
+    let current = --time
+    if (current < 10) {
+      current = `0${current}`
+    }
+    setTime(current)
+  }
+}
+
+function setTime(value) {
+  timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+  timeEl.parentNode.classList.add("hide")
+  board.innerHTML = `<h1>Cчёт: <span class="primary">${score}</span></h2>`
+}
+
+function createRandomCircle() {
+  const circle = document.createElement("div")
+  const size = getRandomNumber(10, 60)
+  const { width, height } = board.getBoundingClientRect()
+  const x = getRandomNumber(0, width - size)
+  const y = getRandomNumber(0, height - size)
+  const color = getRandomColor()
+
+  circle.classList.add("circle")
+  circle.style.width = `${size}px`
+  circle.style.height = `${size}px`
+  circle.style.top = `${y}px`
+  circle.style.left = `${x}px`
+  circle.style.background = color
+
+  board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
+}
